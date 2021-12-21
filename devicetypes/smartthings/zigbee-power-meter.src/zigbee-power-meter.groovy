859bbe666c9a250a6296c35471d6f72e3f407864
TK LEE <ltk016@ezex.co.kr>
2021-12-21 11:16:39 +0900

    fix the indentations by change 4 spaces to 1 tab

--- devicetypes/smartthings/zigbee-power-meter.src/zigbee-power-meter.groovy
+++ devicetypes/smartthings/zigbee-power-meter.src/zigbee-power-meter.groovy
@@ -1,14 +1,14 @@
 /**
- *  Copyright 2019 SmartThings
+ *	Copyright 2019 SmartThings
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *	in compliance with the License. You may obtain a copy of the License at:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *		http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
+ *	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *	for the specific language governing permissions and limitations under the License.
  *
  */
 import physicalgraph.zigbee.zcl.DataType
@@ -44,7 +44,7 @@ metadata {
 			}
 		}
 		standardTile("reset", "device.energy", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-            state "default", label: 'reset kWh', action: "reset"
+			state "default", label: 'reset kWh', action: "reset"
 		}
 		standardTile("refresh", "device.power", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
 			state "default", label: '', action: "refresh.refresh", icon: "st.secondary.refresh"
@@ -82,7 +82,7 @@ def parse(String description) {
 		}
 		log.info "event outer:$event"
 		sendEvent(event)
-    } else {
+	} else {
 		List result = []
 		def descMap = zigbee.parseDescriptionAsMap(description)
 		log.debug "Desc Map: $descMap"
